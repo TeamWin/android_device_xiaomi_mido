66d8989d920d8a8cbd790f2af54715d1e4739098
Yuri Konotopov <ykonotopov@gnome.org>
2018-07-01 23:40:55 +0400

    mido: enable /data partition decryption

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -64,6 +64,7 @@ TARGET_USERIMAGES_USE_EXT4 := true
 # TWRP Configuration
 TW_THEME := portrait_hdpi
 TW_INCLUDE_CRYPTO := true
+TW_CRYPTO_USE_SYSTEM_VOLD := qseecomd
 TW_MAX_BRIGHTNESS := 255
 TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
 TW_EXCLUDE_SUPERSU := true
